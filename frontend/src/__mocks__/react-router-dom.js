ae5380737dbd910706186da34298ead7b3d63ad4
Ruchira Tharanka <150226449+R-Tharanka@users.noreply.github.com>
2025-05-01 20:24:57 +0530

    refactor: enhance mock implementation of react-router-dom with clearer comments and improved useNavigate function

--- frontend/src/__mocks__/react-router-dom.js
+++ frontend/src/__mocks__/react-router-dom.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-// A minimal stub of the bits of React Router you use in your app/tests:
+//
+// A minimal stub of the bits of React Router you use in your app/tests
+//
+
+// We'll hold onto one single navigate mock function, but allow it to be replaced:
+let _navigateMock = () => {};
+export function __setNavigateMock(fn) {
+  _navigateMock = fn;
+}
+
 export function BrowserRouter({ children }) {
   return <>{children}</>;
 }
@@ -13,8 +22,9 @@ export function Routes({ children }) {
 export function Route({ element }) {
   return element;
 }
+// This hook returns whatever _navigateMock currently is:
 export function useNavigate() {
-  return () => {};
+  return _navigateMock;
 }
 export function useParams() {
   return {};
